0e83609e9e7009c79deb466aec25ee6b6b5ca696
김민호 <49149127+rlaalsgh@users.noreply.github.com>
2021-07-19 00:44:09 +0900

    Delphi File Option

---
+++ Practice/FmxProject.dpr
@@ -0,0 +1,14 @@
+program FmxProject;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  FmxForm in 'FmxForm.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
